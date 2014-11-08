5d73cc4e6abce75b2a3e4526cdc95338f1c97d50
Alisson <thelinuxlich@gmail.com>
2014-11-08 04:13:21 -0200

    now using moment-timezone instead of time for compatibility with Windows and Node 0.11.x

--- tests/test-crontime.js
+++ tests/test-crontime.js
@@ -1,209 +1,181 @@
 var testCase = require('nodeunit').testCase,
-    cron = require('../lib/cron');
+    cron = require('../lib/cron'),
+    moment = require('moment-timezone');
 
 module.exports = testCase({
-        'test stars (* * * * * *)': function(assert) {
-            assert.expect(1);
-            assert.doesNotThrow(function() {
-                new cron.CronTime('* * * * * *');
-            });
-            assert.done();
-        },
-        'test digit (0 * * * * *)': function(assert) {
-            assert.expect(1);
-            assert.doesNotThrow(function() {
-                new cron.CronTime('0 * * * * *');
-            });
-            assert.done();
-        },
-        'test multi digits (08 * * * * *)': function(assert) {
-            assert.expect(1);
-            assert.doesNotThrow(function() {
-                new cron.CronTime('08 * * * * *');
-            });
-            assert.done();
-        },
-        'test all digits (08 8 8 8 8 5)': function(assert) {
-            assert.expect(1);
-            assert.doesNotThrow(function() {
-                new cron.CronTime('08 * * * * *');
-            });
-            assert.done();
-        },
-        'test too many digits (08 8 8 8 8 5)': function(assert) {
-            assert.expect(1);
-            assert.doesNotThrow(function() {
-                new cron.CronTime('08 * * * * *');
-            });
-            assert.done();
-        },
-        'test no second digit doesnt throw, i.e. standard cron format (8 8 8 8 5)': function(assert) {
-            assert.expect(1);
-            assert.doesNotThrow(function() {
-                new cron.CronTime('* * * * *');
-            });
-            assert.done();
-        },
-        'test no second digit defaults to 0, i.e. standard cron format (8 8 8 8 5)': function(assert) {
-            assert.expect(1);
-            var now = new Date();
-            var standard = new cron.CronTime('8 8 8 8 5');
-            var extended = new cron.CronTime('0 8 8 8 8 5');
-            assert.ok(standard._getNextDateFrom(now).getTime() === extended._getNextDateFrom(now).getTime());
-            assert.done();
-        },
-        'test hyphen (0-10 * * * * *)': function(assert) {
-            assert.expect(1);
-            assert.doesNotThrow(function() {
-                new cron.CronTime('0-10 * * * * *');
-            });
-            assert.done();
-        },
-        'test multi hyphens (0-10 0-10 * * * *)': function(assert) {
-            assert.expect(1);
-            assert.doesNotThrow(function() {
-                new cron.CronTime('0-10 0-10 * * * *');
-            });
-            assert.done();
-        },
-        'test all hyphens (0-10 0-10 0-10 0-10 0-10 0-1)': function(assert) {
-            assert.expect(1);
-            assert.doesNotThrow(function() {
-                new cron.CronTime('0-10 0-10 0-10 0-10 0-10 0-1');
-            });
-            assert.done();
-        },
-        'test comma (0,10 * * * * *)': function(assert) {
-            assert.expect(1);
-            assert.doesNotThrow(function() {
-                new cron.CronTime('0,10 * * * * *');
-            });
-            assert.done();
-        },
-        'test multi commas (0,10 0,10 * * * *)': function(assert) {
-            assert.expect(1);
-            assert.doesNotThrow(function() {
-                new cron.CronTime('0,10 0,10 * * * *');
-            });
-            assert.done();
-        },
-        'test all commas (0,10 0,10 0,10 0,10 0,10 0,1)': function(assert) {
-            assert.expect(1);
-            assert.doesNotThrow(function() {
-                new cron.CronTime('0,10 0,10 0,10 0,10 0,10 0,1');
-            });
-            assert.done();
-        },
-        'test alias (* * * * jan *)': function(assert) {
-            assert.expect(1);
-            assert.doesNotThrow(function() {
-                new cron.CronTime('* * * * jan *');
-            });
-            assert.done();
-        },
-        'test multi aliases (* * * * jan,feb *)': function(assert) {
-            assert.expect(1);
-            assert.doesNotThrow(function() {
-                new cron.CronTime('* * * * jan,feb *');
-            });
-            assert.done();
-        },
-        'test all aliases (* * * * jan,feb mon,tue)': function(assert) {
-            assert.expect(1);
-            assert.doesNotThrow(function() {
-                new cron.CronTime('* * * * jan,feb mon,tue');
-            });
-            assert.done();
-        },
-        'test unknown alias (* * * * jar *)': function(assert) {
-            assert.expect(1);
-            assert.throws(function() {
-                new cron.CronTime('* * * * jar *');
-            });
-            assert.done();
-        },
-        'test unknown alias - short (* * * * j *)': function(assert) {
-            assert.expect(1);
-            assert.throws(function() {
-                new cron.CronTime('* * * * j *');
-            });
-            assert.done();
-        },
-        'test Date': function(assert) {
-          assert.expect(1);
-          var d = new Date();
-          var ct = new cron.CronTime(d);
-          assert.equals(ct.source.getTime(), d.getTime());
-          assert.done();
-        },
-        'test day roll-over': function(assert) {
-          var numHours = 24;
-          assert.expect(numHours * 2);
-          var ct = new cron.CronTime('0 0 17 * * *');
-          
-          for (var hr = 0; hr < numHours; hr++) {
+    'test stars (* * * * * *)': function(assert) {
+        assert.expect(1);
+        assert.doesNotThrow(function() {
+            new cron.CronTime('* * * * * *');
+        });
+        assert.done();
+    },
+    'test digit (0 * * * * *)': function(assert) {
+        assert.expect(1);
+        assert.doesNotThrow(function() {
+            new cron.CronTime('0 * * * * *');
+        });
+        assert.done();
+    },
+    'test multi digits (08 * * * * *)': function(assert) {
+        assert.expect(1);
+        assert.doesNotThrow(function() {
+            new cron.CronTime('08 * * * * *');
+        });
+        assert.done();
+    },
+    'test all digits (08 8 8 8 8 5)': function(assert) {
+        assert.expect(1);
+        assert.doesNotThrow(function() {
+            new cron.CronTime('08 * * * * *');
+        });
+        assert.done();
+    },
+    'test too many digits (08 8 8 8 8 5)': function(assert) {
+        assert.expect(1);
+        assert.doesNotThrow(function() {
+            new cron.CronTime('08 * * * * *');
+        });
+        assert.done();
+    },
+    'test no second digit doesnt throw, i.e. standard cron format (8 8 8 8 5)': function(assert) {
+        assert.expect(1);
+        assert.doesNotThrow(function() {
+            new cron.CronTime('* * * * *');
+        });
+        assert.done();
+    },
+    'test no second digit defaults to 0, i.e. standard cron format (8 8 8 8 5)': function(assert) {
+        assert.expect(1);
+        var now = new Date();
+        var standard = new cron.CronTime('8 8 8 8 5');
+        var extended = new cron.CronTime('0 8 8 8 8 5');
+        assert.ok(standard._getNextDateFrom(now).valueOf() === extended._getNextDateFrom(now).valueOf());
+        assert.done();
+    },
+    'test hyphen (0-10 * * * * *)': function(assert) {
+        assert.expect(1);
+        assert.doesNotThrow(function() {
+            new cron.CronTime('0-10 * * * * *');
+        });
+        assert.done();
+    },
+    'test multi hyphens (0-10 0-10 * * * *)': function(assert) {
+        assert.expect(1);
+        assert.doesNotThrow(function() {
+            new cron.CronTime('0-10 0-10 * * * *');
+        });
+        assert.done();
+    },
+    'test all hyphens (0-10 0-10 0-10 0-10 0-10 0-1)': function(assert) {
+        assert.expect(1);
+        assert.doesNotThrow(function() {
+            new cron.CronTime('0-10 0-10 0-10 0-10 0-10 0-1');
+        });
+        assert.done();
+    },
+    'test comma (0,10 * * * * *)': function(assert) {
+        assert.expect(1);
+        assert.doesNotThrow(function() {
+            new cron.CronTime('0,10 * * * * *');
+        });
+        assert.done();
+    },
+    'test multi commas (0,10 0,10 * * * *)': function(assert) {
+        assert.expect(1);
+        assert.doesNotThrow(function() {
+            new cron.CronTime('0,10 0,10 * * * *');
+        });
+        assert.done();
+    },
+    'test all commas (0,10 0,10 0,10 0,10 0,10 0,1)': function(assert) {
+        assert.expect(1);
+        assert.doesNotThrow(function() {
+            new cron.CronTime('0,10 0,10 0,10 0,10 0,10 0,1');
+        });
+        assert.done();
+    },
+    'test alias (* * * * jan *)': function(assert) {
+        assert.expect(1);
+        assert.doesNotThrow(function() {
+            new cron.CronTime('* * * * jan *');
+        });
+        assert.done();
+    },
+    'test multi aliases (* * * * jan,feb *)': function(assert) {
+        assert.expect(1);
+        assert.doesNotThrow(function() {
+            new cron.CronTime('* * * * jan,feb *');
+        });
+        assert.done();
+    },
+    'test all aliases (* * * * jan,feb mon,tue)': function(assert) {
+        assert.expect(1);
+        assert.doesNotThrow(function() {
+            new cron.CronTime('* * * * jan,feb mon,tue');
+        });
+        assert.done();
+    },
+    'test unknown alias (* * * * jar *)': function(assert) {
+        assert.expect(1);
+        assert.throws(function() {
+            new cron.CronTime('* * * * jar *');
+        });
+        assert.done();
+    },
+    'test unknown alias - short (* * * * j *)': function(assert) {
+        assert.expect(1);
+        assert.throws(function() {
+            new cron.CronTime('* * * * j *');
+        });
+        assert.done();
+    },
+    'test Date': function(assert) {
+        assert.expect(1);
+        var d = new Date();
+        var ct = new cron.CronTime(d);
+        assert.equals(ct.source.valueOf(), d.getTime());
+        assert.done();
+    },
+    'test day roll-over': function(assert) {
+        var numHours = 24;
+        assert.expect(numHours * 2);
+        var ct = new cron.CronTime('0 0 17 * * *');
+
+        for (var hr = 0; hr < numHours; hr++) {
             var start = new Date(2012, 3, 16, hr, 30, 30);
             var next = ct._getNextDateFrom(start);
-            assert.ok(next - start < 24*60*60*1000);
+            assert.ok(next - start < 24 * 60 * 60 * 1000);
             assert.ok(next > start);
-          }
-          assert.done();
-        },
-        'test illegal repetition syntax': function(assert){
-          assert.throws(function(){
+        }
+        assert.done();
+    },
+    'test illegal repetition syntax': function(assert) {
+        assert.throws(function() {
             new cron.CronTime('* * /4 * * *');
-          });
-          assert.done();
-				},
-				'test next date': function(assert) {
-          assert.expect(2);
-          var ct = new cron.CronTime('0 0 */4 * * *');
+        });
+        assert.done();
+    },
+    'test next date': function(assert) {
+        assert.expect(2);
+        var ct = new cron.CronTime('0 0 */4 * * *');
 
-					var nextDate = new Date();
-					nextDate.setHours(23);
-					var nextdt = ct._getNextDateFrom(nextDate);
-					assert.ok(nextdt > nextDate);
-					assert.ok(nextdt.getHours() % 4 === 0);
-					assert.done();
-				},
-				'test next real date': function(assert) {
-          assert.expect(2);
-          var ct = new cron.CronTime(new Date());
+        var nextDate = new Date();
+        nextDate.setHours(23);
+        var nextdt = ct._getNextDateFrom(nextDate);
+        assert.ok(nextdt > nextDate);
+        assert.ok(nextdt.hours() % 4 === 0);
+        assert.done();
+    },
+    'test next real date': function(assert) {
+        assert.expect(2);
+        var ct = new cron.CronTime(new Date());
 
-					var nextDate = new Date();
-					nextDate.setMonth(nextDate.getMonth()+1);
-					assert.ok(nextDate > ct.source);
-					var nextdt = ct._getNextDateFrom(nextDate);
-					assert.deepEqual(nextdt, nextDate);
-					assert.done();
-				},
-				'test < constraints day of month': function(assert) {
-					assert.expect(5);
-
-					var ltm = [1, 3, 5, 8, 10];
-					for (var i = 0; i < ltm.length; i++) {
-						(function(m) {
-							assert.throws(function() {
-								var ct = new cron.CronTime('0 0 0 33 ' + m + ' *');
-							});
-						})(ltm[i]);
-					}
-
-					assert.done();
-				},
-				'test next month selection': function(assert) {
-					assert.expect(1);
-					var date = new Date();
-					var dom = date.getDate() + 1;
-					var ct = new cron.CronTime('0 0 0 ' + dom + ' * *');
-
-					var saDate = ct.sendAt();
-
-					if (dom < date.getDate())
-						date.setMonth(date.getMonth()+1);
-
-					assert.equal(date.getMonth(), saDate.getMonth());
-
-					assert.done()
-				}
+        var nextDate = moment();
+        nextDate.month(nextDate.month() + 1);
+        assert.ok(nextDate > ct.source);
+        var nextdt = ct._getNextDateFrom(nextDate);
+        assert.deepEqual(nextdt, nextDate);
+        assert.done();
+    }
 });
