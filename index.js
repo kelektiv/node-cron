f2e3456e3d406b9d382821babfebccaf947a5ccc
Luciano Peranni <lucianoperanni95@gmail.com>
2020-03-09 15:18:43 +0100

    Clean up

--- index.js
+++
@@ -1,7 +0,0 @@
-const { CronTime } = require('./lib/cron');
-
-const schedule = new CronTime(
-	'{"second": "0", "hour": "16", "minute": "15","dayOfWeek": "fri", "month": "jan"}'
-);
-
-console.log(schedule);
