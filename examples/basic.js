1d466e6489007d56d34d89b1aa22427c77c3283b
Imran Nur Hirey <95400512+imranhirey@users.noreply.github.com>
2022-09-18 20:14:41 +0000

    Update basic.js

--- examples/basic.js
+++ examples/basic.js
@@ -1,9 +1,11 @@
 const CronJob = require('../lib/cron.js').CronJob;
 
-console.log('Before job instantiation');
+console.log('Before job instantiation at node-cron');
 const job = new CronJob('* * * * * *', function() {
 	const d = new Date();
 	console.log('Every second:', d);
+	if (nodeciron==2){
+		return 3 else 7
 });
 console.log('After job instantiation');
 job.start();
