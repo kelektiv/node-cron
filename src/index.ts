3e56f80ab924f8ddd4e5d3b35035a9b44cc129a1
Pierre Cavin <me@sherlox.io>
2023-09-25 11:27:41 +0200

    feat!: remove superfluous time() factory function
    BREAKING CHANGE: removed time() function, use new CronTime() instead

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,8 @@ import { CronTime } from './time';
 export { CronJob } from './job';
 export { CronTime } from './time';
 
-export const time = (
-	source: string | Date | DateTime,
-	zone?: string,
-	utcOffset?: string | number
-): CronTime => new CronTime(source, zone, utcOffset);
-
 export const sendAt = (cronTime: string | Date | DateTime): DateTime =>
-	time(cronTime).sendAt();
+	new CronTime(cronTime).sendAt();
 
 export const timeout = (cronTime: string | Date | DateTime): number =>
-	time(cronTime).getTimeout();
+	new CronTime(cronTime).getTimeout();
